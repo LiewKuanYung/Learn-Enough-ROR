0658d09594c97ed58707d8df9f5077ab9a0c6020
Liew Kuan Yung <liewkuanyung@gmail.com>
2022-03-20 18:27:18 +0800

    Feat: finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
-  def new
+  
+  def show
+    @user = User.find(params[:id])
+    # debugger
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # a message that appears on the subsequent page 
+      # and then disappears upon visiting a second page or on page reload.
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user # equivalent to redirect_to user_url(@user)
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, 
+                                   :email, 
+                                   :password, 
+                                   :password_confirmation)
+    end
 end
